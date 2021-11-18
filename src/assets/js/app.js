387df9e8211c4c52ed7f6e49d314f3c645221c7a
Magomed Alimirzaev <magomet266@gmail.com>
2021-11-18 14:00:29 +0300

    november 18

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,36 @@
-console.log('file 1');
+
+let menu_body = document.getElementById("bur");
+document.querySelector(".burger-menu").addEventListener('click',()=>{
+    
+    if(menu_body.classList.value === "burger-menu--v" ){
+        menu_body.classList.toggle("burger-menu--v");
+        menu_body.classList.add("burger-menu--h")
+    }else{
+        menu_body.classList.remove("burger-menu--h");
+        menu_body.classList.add("burger-menu--v");
+    };
+});
+
+let form = document.querySelector(".discussion-form-answer--h");
+let elements = [...document.querySelectorAll('#click')].forEach(item => { 
+   
+    item.addEventListener('click',()=>{
+        if(elements.classList.value === "discussion-form-answer--h" ){
+            elements.classList.remove("discussion-form-answer--h");
+            elements.classList.add("discussion-form-answer")
+        }else{
+            elements.classList.remove("discussion-form-answer");
+            elements.classList.add("discussion-form-answer--h");
+        };
+      });
+  });
+
+  let modalWindow = document.querySelector(".modal-container");
+  let body = document.querySelector("body");
+  body.addEventListener("click",()=>{
+      modalWindow.classList.remove("modal-container")
+      document.querySelector(".story-modal").classList.add("story-modal--hidden")
+
+
+  })
+  
